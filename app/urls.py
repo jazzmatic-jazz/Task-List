a970b15a1ebf8fb9264a93d65b0180bd162e197e
jazzmatic-jazz <jazminemazzey@gmail.com>
2024-09-06 12:39:09 +0530

    Add: templates

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import task_list
+from .views import task_list, user_login
+
 
 
 urlpatterns = [
     path("", task_list, name="task_list"),
+    path("login", user_login, name='login'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,39 +1,58 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
 from .models import User, Task
+from .choices import STATUS
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 
 def task_list(request):
-    # Get filter and sorting parameters from the request
-    status = request.POST.get('status')
-    sort_by = request.POST.get('sort_by')
+    '''
+        Displays the Task List from the Database
+        according to the filters and sorting based 
+        on statuses, priorities and due dates of tasks
+        renders template
+        - app/task_list.html
 
-    print("status", status)
-    print("sort_by", sort_by)
-    # Filter tasks by status if provided
-    if status:
-        tasks = Task.objects.filter(status=status)
-    else:
-        tasks = Task.objects.all()
+    '''
+    tasks = Task.objects.all()
 
-    # Sort tasks by priority or due date if provided
-    if sort_by == 'priority':
-        tasks = tasks.order_by('priority')
-    elif sort_by == 'due_date':
-        tasks = tasks.order_by('due_date')
+    if request.method == 'POST':
 
-    status_options = Task.objects.values_list('status', flat=True).distinct()
-    priority_options = Task.objects.values_list('priority', flat=True).distinct()
-    print("status_options", status_options)
-    print("priority_options", priority_options)
+        get_status = request.POST.get('status')
+        get_sort_by = request.POST.get('sort_by')
+
+        # tasks = Task.objects.all()
+
+        get_status = None if get_status in [None, "None", ""] else get_status
+        get_sort_by = None if get_sort_by in [None, "None", ""] else get_sort_by
+
+        if get_status is None and get_sort_by is None:
+            return render(request, 'app/task_list.html', context={"tasks": tasks, "render_status": STATUS})
+        
+        if get_status:
+            tasks = Task.objects.filter(status=get_status)
+        
+        if get_sort_by == 'priority':
+            tasks = tasks.order_by('priority')
+        
+        if get_sort_by == 'due_date':
+            tasks = tasks.order_by('due_date')
+
+        return render(request, 'app/task_list.html', context={"tasks": tasks, "render_status": STATUS})
     
-
-    context = {
-        'tasks': tasks,
-        'statuses': status_options,
-        'priority_options': priority_options
-    }
-
-    return render(request, 'app/task_list.html', context={"tasks": tasks})
+    return render(request, 'app/task_list.html', context={"tasks": tasks, "render_status": STATUS} )
 
 
+def user_login(request):
+    if request.method == 'POST':
+        get_email = request.POST.get('email')
+        get_password = request.POST.get('password')
+
+        user = authenticate(request, username=get_email, password=get_password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/task')
+
+    messages.error(request, 'Invalid username or password.')
+    return render(request, 'app/login.html')
